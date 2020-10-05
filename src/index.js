6506d35185a9497b5d78cbef85e1b80e8f95933f
Hramchenlo Alexey <qwefix@gmail.com>
2020-10-05 19:24:29 +0300

    feat: revere-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let resArr=[];
+  let arrOfN=n.toString().split('');
+  if(n<0){      
+      arrOfN.splice(0,1);
+  }
+  for(let i=arrOfN.length-1;i>=0;i--){
+      resArr.push(arrOfN[i]);
+  }
+  return +(resArr.join(''));
 }
